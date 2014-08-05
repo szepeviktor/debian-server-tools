582fb1dd3e0433c6c6b73bad6c9c7d8c9842b176
Szépe Viktor <viktor@szepe.net>
2014-08-05 20:23:26 +0200

    Update hdd-bench.sh

--- monitoring/hdd-seeker/hdd-bench.sh
+++ monitoring/hdd-seeker/hdd-bench.sh
@@ -6,25 +6,41 @@ which gcc &> /dev/null || exit 1
 # https://github.com/baryluk
 gcc -pthread -o seeker seeker_baryluk.c
 # https://github.com/mlsorensen/seekmark
-gcc -pthread -o seekmark seekmark-0.9.c
+gcc -pthread -o seekmark seekmark-0.9.1.c
 
+# VMware
 DEVICE="/dev/sda"
+# XEN
 [ -b "$DEVICE" ] || DEVICE="/dev/xvda"
-[ -b "$DEVICE" ] || read -p "Disk to test? " DEVICE
-[ -b "$DEVICE" ] || exit 2
+# first block device
+if ! [ -b "$DEVICE" ]; then
+    DEVICE="/dev/$(tail -n +3 /proc/partitions 2>/dev/null | head -n 1 | tr ' ' $'\n' | tail -n 1)"
+    read -e -i "$DEVICE" -p "Disk to test? " DEVICE
+fi
+[ -r "$DEVICE" ] || exit 2
 
+# SeekMark ~ 30 sec
+ONESEC=$(./seekmark -f "$DEVICE" -t 2 -s 1000 \
+    | grep -o ", [0-9.]* READ seeks per sec per thread$" | grep -o "[0-9]*" | head -n 1)
 
-echo;echo DIRECT benchmark
-./seeker $DEVICE 2
+echo;echo "DIRECT benchmark (new)"
+./seekmark -f "$DEVICE" -t 2 -s $((ONESEC * 30))
+echo ------------------------------------
 
-echo;echo DIRECT benchmark
-./seeker $DEVICE 2
+# seeker and hdparm reads only block devices
+[ -b "$DEVICE" ] || exit 0
 
+echo;echo "DIRECT benchmark"
+./seeker "$DEVICE" 2
+echo ------------------------------------
+
+# hdparm
 which hdparm &> /dev/null || exit 0
 
-echo;echo BUFFERED benchmark
-hdparm -t $DEVICE
+echo;echo "BUFFERED benchmark"
+hdparm -t "$DEVICE"
+echo ------------------------------------
 
-echo;echo CACHED benchmark
-hdparm -T $DEVICE
+echo;echo "CACHED benchmark"
+hdparm -T "$DEVICE"
 
