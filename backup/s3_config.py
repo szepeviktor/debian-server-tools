1dcc62fc534cb5840c103cde07eec96578b9b1b8
Viktor Szépe <viktor@szepe.net>
2014-08-06 06:31:27 +0200

    separate s3 config

---
+++ backup/s3_config.py
@@ -0,0 +1,9 @@
+AWS_ACCESS_KEY = '<aws access key>'
+AWS_SECRET_KEY = '<aws secret key>'
+AWS_BUCKET_NAME = '<aws bucket name>'
+# 365 days
+AWS_HEADERS = {
+    'Cache-Control':'max-age=31536000, public'
+}
+AWS_ACL = 'public-read'
+
