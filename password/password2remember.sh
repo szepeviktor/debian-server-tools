85a6e87ec39509d689e5ace73ea8ea07deffcd31
root <root@false>
2014-08-27 15:19:54 +0200

    password2remember

---
+++ password/password2remember.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Generate 8 easy to remember passwords.
+# First option is the acrostic word, second is a number to append.
+# Set P2R_LANG to any language name after you added the corresponding wordlist file.
+# The fixed delimiter is period (the `-d` option of xkcdpass)
+#
+# VERSION       :0.1
+# DATE          :2014-08-27
+# AUTHOR        :Viktor Szépe <viktor@szepe.net>
+# LICENSE       :The MIT License (MIT)
+# URL           :https://github.com/szepeviktor/debian-server-tools
+# BASH-VERSION  :4.2+
+# DEPENDS       :pip install xkcdpass
+# WORDLIST_HU   :http://packetstormsecurity.com/files/32010/hungarian.gz.html
+# WORDLIST_HU2  :http://sourceforge.net/projects/wordlist-hu/
+
+P2R_LANG="hu"
+
+
+ACROSTIC="$1"
+NUMBER="$2"
+
+capitalize() {
+    local RATIO="$1"
+    # bash RANDOM is in 0..32767
+    local CAP_LIMIT="$(( 32767 / RATIO ))"
+
+    while read -n 1 LETTER; do
+        echo -n "$LETTER" \
+            | if [ "$RANDOM" -lt "$CAP_LIMIT" ]; then
+                tr '[:lower:]' '[:upper:]'
+            else
+                cat
+            fi
+    done
+}
+
+[ -z "$ACROSTIC" ] || echo "a.c.r.o.s.t.i.c.: ${ACROSTIC}"
+[ -z "$NUMBER" ] || echo "number: ${NUMBER}"
+
+# 8 password offers
+for I in $(seq 1 8); do
+    XKCDPASS=$(xkcdpass -d . -w "password2remember_${P2R_LANG}.txt" -n 4 --max=7 -a "$ACROSTIC")
+
+    # capitalize in 1/6 ratio
+    echo "$XKCDPASS" | capitalize 6
+
+    echo "$NUMBER"
+done
