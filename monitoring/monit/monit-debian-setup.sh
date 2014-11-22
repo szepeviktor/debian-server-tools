fdc20ea57679f893d4bf449f56c17fa99887cb19
Viktor Szépe <viktor@szepe.net>
2014-11-22 22:57:02 +0100

    monit

---
+++ monitoring/monit/monit-debian-setup.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+#
+# Configure up monit plugins
+# These variables need to be filled in.
+#
+
+MONIT_BOOT_DELAY="120"
+MONIT_EMAIL_HOST=""
+MONIT_FULL_HOSTNAME=""
+MONIT_SSH_PORT=""
+MONIT_PHPFPM_SOCKET=""
+
+Monit_enable() {
+    local PLUGIN="$1"
+
+    if ! [ -f "/etc/monit/monitrc.d/${PLUGIN}" ]; then
+        echo "Plugin not found: (${PLUGIN})" >&2
+        return 1
+    fi
+
+    ln -svf "/etc/monit/monitrc.d/${PLUGIN}" "/etc/monit/conf.d/${PLUGIN}" \
+        || echo "Cannot create symlink" >&2
+}
+
+[ -d /etc/monit/monitrc.d ] || exit 1
+
+## main configuration file
+cat > "/etc/monit/monitrc.d/00_monitrc" <<MONITMAIN
+# https://wiki.debian.org/monit
+# https://mmonit.com/monit/documentation/monit.html
+
+set daemon ${MONIT_BOOT_DELAY}
+    with start delay ${MONIT_BOOT_DELAY}
+
+# alert emails
+set mailserver localhost port 25
+set mail-format { from: root@${MONIT_EMAIL_HOST} }
+set alert root@${MONIT_EMAIL_HOST}
+
+# web interface
+set httpd port 2812 and
+    use address localhost
+    allow localhost
+MONITMAIN
+#########
+
+## system
+cat > "/etc/monit/monitrc.d/01_${MONIT_FULL_HOSTNAME}" <<MONITSYSTEM
+check system ${MONIT_FULL_HOSTNAME//[^0-9A-Za-z]/_}
+    if loadavg (1min) > 4 then alert
+    if loadavg (5min) > 2 then alert
+    if memory usage > 75% then alert
+    if swap usage > 25% then alert
+    if cpu usage (user) > 70% then alert
+    if cpu usage (system) > 30% then alert
+    if cpu usage (wait) > 20% then alert
+MONITSYSTEM
+###########
+
+## sshd
+sed -i "s/port 22 with proto ssh/port ${MONIT_SSH_PORT} with proto ssh/" /etc/monit/monitrc.d/openssh-server
+
+## unscd
+cat > "/etc/monit/monitrc.d/unscd" <<MONITUNSCD
+# µNameservice caching daemon (unscd)
+check process nscd with pidfile /var/run/nscd/nscd.pid
+    group system
+    start program = "/etc/init.d/unscd start"
+    stop  program = "/etc/init.d/unscd stop"
+    if 5 restarts within 5 cycles then timeout
+    depends on nscd_bin
+    depends on nscd_rc
+
+check file nscd_bin with path /usr/sbin/nscd
+    group system
+    if failed permission 755 then unmonitor
+    if failed uid root then unmonitor
+    if failed gid root then unmonitor
+
+check file nscd_rc with path /etc/init.d/unscd
+    group system
+    if failed permission 755 then unmonitor
+    if failed uid root then unmonitor
+    if failed gid root then unmonitor
+MONITUNSCD
+##########
+
+## fail2ban
+cat > "/etc/monit/monitrc.d/fail2ban" <<MONITFAIL2BAN
+check process fail2ban with pidfile /var/run/fail2ban/fail2ban.pid
+    group services
+    start program = "/etc/init.d/fail2ban start"
+    stop  program = "/etc/init.d/fail2ban stop"
+    if failed unixsocket /var/run/fail2ban/fail2ban.sock then restart
+    if 5 restarts within 5 cycles then timeout
+
+check file fail2ban_log with path /var/log/fail2ban.log
+    if match "ERROR|WARNING" then alert
+MONITFAIL2BAN
+#############
+
+
+## enable custom plugins
+Monit_enable 00_monitrc
+Monit_enable "01_${MONIT_FULL_HOSTNAME}"
+Monit_enable unscd
+Monit_enable fail2ban
+
+## enable contributed plugins
+# https://github.com/perusio/monit-miscellaneous
+wget "https://raw.githubusercontent.com/perusio/monit-miscellaneous/master/php-fpm-unix"
+sed -i 's|unixsocket /var/run/php-fpm.sock then|unixsocket /var/run/${MONIT_PHPFPM_SOCKET} then|' php-fpm-unix
+Monit_enable php-fpm-unix
+# http://storage.fladi.at/~FladischerMichael/monit/
+# mirror: https://github.com/szepeviktor/FladischerMichael.monit
+wget -O courier "https://raw.githubusercontent.com/szepeviktor/FladischerMichael.monit/master/courier.test"
+Monit_enable courier
+wget -O courier-auth "https://github.com/szepeviktor/FladischerMichael.monit/raw/master/courier-auth.test"
+Monit_enable courier-auth
+if [ -x /usr/bin/imapd ]; then
+    wget -O courier-imap "https://github.com/szepeviktor/FladischerMichael.monit/raw/master/courier-imap.test"
+    Monit_enable courier-imap
+fi
+# more: https://extremeshok.com/5207/monit-configs-for-ubuntu-debian-centos-rhce-redhat/
+
+## enable built-in plugins
+Monit_enable apache2
+Monit_enable cron
+Monit_enable mysql
+Monit_enable openssh-server
+Monit_enable rsyslog
+
+## enable built-in plugins - hardware related
+#Monit_enable acpid
+#Monit_enable smartmontools
+#Monit_enable mdadm
+
+## enable built-in plugins - others
+#Monit_enable at
+#Monit_enable memcached
+#Monit_enable nginx
+#Monit_enable openntpd
+#Monit_enable pdns-recursor
+#Monit_enable postfix
+#Monit_enable snmpd
