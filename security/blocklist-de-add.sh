551091f55ff8aebdaf1bdf86c1521d4f694dbdb3
Viktor Szépe <viktor@szepe.net>
2015-02-19 11:16:25 +0100

    iptables path fixed

--- security/blocklist-de-add.sh
+++ security/blocklist-de-add.sh
@@ -2,16 +2,24 @@
 #
 # Add blocklist.de's list for very abusive IP-s to iptables.
 #
-# VERSION       :0.1
-# DATE          :2015-02-18
+# VERSION       :0.2
+# DATE          :2015-02-19
 # AUTHOR        :Viktor Szépe <viktor@szepe.net>
 # LICENSE       :The MIT License (MIT)
 # URL           :https://github.com/szepeviktor/debian-server-tools
 # BASH-VERSION  :4.2+
 # LOCATION      :/usr/local/bin/blocklist-de-add.sh
+# CRON-D        :10 7  * * *  root  /usr/local/sbin/blocklist-de-add.sh
 
 A5K_URL="http://lists.blocklist.de/lists/strongips.txt"
 A5K_CHAIN="ATTACKER5K"
+IPTABLES="/sbin/iptables"
+
+# check iptables executable
+if ! [ -x "$IPTABLES" ]; then
+    echo "iptables executable not found (${IPTABLES})" >&2
+    exit 1
+fi
 
 # check list integrity
 A5K_MD5="$(wget -qO- "${A5K_URL}.md5")"
@@ -21,7 +29,7 @@ wget -qO "$A5K_TMP" "$A5K_URL"
 if ! [ -s "$A5K_TMP" ]; then
     echo "blocklist.de's strongips list update failed." >&2
     rm "$A5K_TMP"
-    exit 1
+    exit 2
 fi
 
 A5K_LIST_MD5="$(md5sum "$A5K_TMP")"
@@ -29,23 +37,23 @@ A5K_LIST_MD5="${A5K_LIST_MD5%% *}"
 if [ "$A5K_LIST_MD5" != "$A5K_MD5" ]; then
     echo "blocklist.de's strongips list integrity failed." >&2
     rm "$A5K_TMP"
-    exit 2
+    exit 3
 fi
 
 # remove from INPUT for now
-iptables -D INPUT -j "$A5K_CHAIN" &> /dev/null
+"$IPTABLES" -D INPUT -j "$A5K_CHAIN" &> /dev/null
 
 # set up chain and rules
-iptables -N "$A5K_CHAIN" &> /dev/null
-iptables -F "$A5K_CHAIN"
+"$IPTABLES" -N "$A5K_CHAIN" &> /dev/null
+"$IPTABLES" -F "$A5K_CHAIN"
 while read A5K; do
-    iptables -A "$A5K_CHAIN" -s "$A5K" -j DROP
+    "$IPTABLES" -A "$A5K_CHAIN" -s "$A5K" -j DROP
 done < "$A5K_TMP"
-iptables -A "$A5K_CHAIN" -j RETURN
+"$IPTABLES" -A "$A5K_CHAIN" -j RETURN
 
 # add back to INPUT
-iptables -C INPUT -j "$A5K_CHAIN" &> /dev/null || iptables -I INPUT -j "$A5K_CHAIN"
+"$IPTABLES" -C INPUT -j "$A5K_CHAIN" &> /dev/null || "$IPTABLES" -I INPUT -j "$A5K_CHAIN"
 
 rm "$A5K_TMP"
 
-tty --quiet && iptables -n -L "$A5K_CHAIN" | grep -w "^DROP" | wc -l
+tty --quiet && "$IPTABLES" -n -L "$A5K_CHAIN" | grep -w "^DROP" | wc -l
