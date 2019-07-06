b387694676ee92b0426c36c91f7b263e6cd66ac8
Viktor Szépe <viktor@szepe.net>
2019-07-06 10:59:47 +0000

    daily

---
+++ webserver/laravel/config/logging.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Use Laravel logging to communicate with the Firewall.
+ *
+ * Usage: Log::channel('firewall')->error('Malicious traffic detected: laravel_' . $eventSlug, $contextArray);
+ */
+
+return [
+    'channels' => [
+
+        'firewall' => [
+            'driver' => 'monolog',
+            'handler' => Monolog\Handler\ErrorLogHandler::class,
+            'level' => 'error',
+            'formatter' => Monolog\Formatter\LineFormatter::class,
+            'formatter_with' => [
+                'format' => '%message% %context%',
+            ],
+        ],
+    ]
+];
