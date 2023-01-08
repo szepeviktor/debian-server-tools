623b3d0ff46d4af6040873058eb4e99621769d0b
Flavio Garcia <piraz@candango.org>
2023-01-08 14:21:26 -0500

    Move import from future to the beginning of the script.
    Fix: #13

--- security/jwk_convert.py
+++ security/jwk_convert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 """Convert certbot private_key.json to manuale's account.json
 Source: https://gist.github.com/JonLundy/f25c99ee0770e19dc595
-
+``
 ./jwk_convert.py private_key.json > private-key.asn1
 openssl asn1parse -genconf private-key.asn1 -noout -out private-key.der
 openssl rsa -inform DER -in private-key.der -outform PEM -out private-key.key
@@ -10,13 +10,12 @@ paste -s -d '|' private-key.key | sed -e 's/|/\\n/g' | tr -d '\n' >> account.jso
 echo '", "uri": "https://acme-v01.api.letsencrypt.org/acme/reg/9999999"}' >> account.json # From regr.json
 """
 
+from __future__ import print_function
+
 import sys
 import json
 import base64
 import binascii
-
-if sys.version_info.major == 2:
-    from __future__ import print_function
 
 with open(sys.argv[1]) as fp:
     PKEY = json.load(fp)
@@ -31,17 +30,18 @@ def enc(data):
     return b'0x' + binascii.hexlify(base64.b64decode(data, b'-_')).upper()
 
 
-for k, v in PKEY.items():
-    if k == 'kty':
-        continue
-    PKEY[k] = enc(v.encode())
+if __name__ == '__main__':
+    for k, v in PKEY.items():
+        if k == 'kty':
+            continue
+        PKEY[k] = enc(v.encode())
 
-print("asn1=SEQUENCE:private_key\n[private_key]\nversion=INTEGER:0")
-print("n=INTEGER:{}".format(PKEY[u'n']))
-print("e=INTEGER:{}".format(PKEY[u'e']))
-print("d=INTEGER:{}".format(PKEY[u'd']))
-print("p=INTEGER:{}".format(PKEY[u'p']))
-print("q=INTEGER:{}".format(PKEY[u'q']))
-print("dp=INTEGER:{}".format(PKEY[u'dp']))
-print("dq=INTEGER:{}".format(PKEY[u'dq']))
-print("qi=INTEGER:{}".format(PKEY[u'qi']))
+    print("asn1=SEQUENCE:private_key\n[private_key]\nversion=INTEGER:0")
+    print("n=INTEGER:{}".format(PKEY['n'].decode()))
+    print("e=INTEGER:{}".format(PKEY['e'].decode()))
+    print("d=INTEGER:{}".format(PKEY['d'].decode()))
+    print("p=INTEGER:{}".format(PKEY['p'].decode()))
+    print("q=INTEGER:{}".format(PKEY['q'].decode()))
+    print("dp=INTEGER:{}".format(PKEY['dp'].decode()))
+    print("dq=INTEGER:{}".format(PKEY['dq'].decode()))
+    print("qi=INTEGER:{}".format(PKEY['qi'].decode()))
