85ec5fb9a051e24e719cf6669b479c801465f713
Viktor Szépe <viktor@szepe.net>
2025-11-03 10:17:08 +0000

    daily

--- webserver/generic-php/non-ascii-hu.sh
+++ webserver/generic-php/non-ascii-hu.sh
@@ -25,15 +25,14 @@ ALPHABET="ÁÉÍÓÖŐÚÜŰáéíóöőúüű"
 #ALPHABET="ČĆĐŠŽčćđšž"
 
 # UNICODE Latin-1 Supplement signs (00A1-00BF)
-#ALPHABET="$(for C in A{1..9} A{A..F} B{0..9} B{A..F};do printf "\\x${C}\\x00";done|iconv -f UNICODE -t UTF-8)"
+#ALPHABET="$(for C in A{{1..9},{A..F}} B{{0..9},{A..F}};do printf "\\x${C}\\x00";done|iconv -f UNICODE -t UTF-8)"
 # UNICODE Latin-1 Supplement letter (00C0-00FF)
-#ALPHABET="$(for P in {C..F};do for C in ${P}{0..9} ${P}{A..F};do printf "\\x${C}\\x00";done;done|iconv -f UNICODE -t UTF-8)"
+#ALPHABET="$(for C in {C..F}{{0..9},{A..F}};do printf "\\x${C}\\x00";done|iconv -f UNICODE -t UTF-8)"
 
 # Russian alphabet
 #ALPHABET="АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя"
 # Russian alphabet from UNICODE Cyrillic (0410-042F 0430-044F + 0401 + 0451)
-#ALPHABET="$({ for P in {1..4};do for C in ${P}{0..9} ${P}{A..F};do
-#    printf "\\x${C}\\x04";done;done;printf '\x01\x04\x51\x04'; }|iconv -f UNICODE -t UTF-8)"
+#ALPHABET="$(for C in {1..4}{{0..9},{A..F}} 01 51;do printf "\\x${C}\\x04";done|iconv -f UNICODE -t UTF-8)"
 
 # Punctuation marks in documents
 #PUNCTUATION_MARKS="’“”«»•…–"
